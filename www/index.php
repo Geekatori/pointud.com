e1e23b0ebb651dd722cd887ef469560714f5978e
Mathieu Pointud <mathieu.pointud@periscopemail.com>
2017-08-21 18:14:24 +0200

    trailing white space

--- www/index.php
+++ www/index.php
@@ -58,7 +58,7 @@ $config->load('./config/config.php');
                         Je suis <h3>Intégrateur HTML CSS JS</h3> avec 10 ans d'expérience et je travaille en agence depuis 7 ans. Je travaille sur des projets pour de l'événementiel, grands-comptes, e-commerces ou institutionnels. J'ai un grand soucis du détail pour construire des sites durables, maintenables, responsives, accessibles et optimisés SEO. J'utilise quotidiennement <h3>SASS</h3>, <h3>JavaScript</h3> / <h3>jQuery</h3> et <h3>Gulp</h3>.
                         <br>
                         J'ai aussi une passion pour le vélo.
-                        <a href="pdf/mathieu-pointud.pdf" title="Télécharger le CV de Mathieu Pointud" class="btn btn-default" id="download-resume">Télécharger mon CV</a>
+                        <a href="pdf/cv-mathieu-pointud.pdf" title="Télécharger le CV de Mathieu Pointud" class="btn btn-default" id="download-resume">Télécharger mon CV</a>
                     </div>
                 </div>
             </div>
